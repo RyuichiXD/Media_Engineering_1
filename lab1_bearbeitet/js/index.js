1a52237aa50946037647d01dfd919020a05b184f
Ryuichi001 <samed.sulanc@yahoo.de>
2016-11-18 15:51:41 +0100

    Lab 2.1,2.2,2.3

---
+++ lab1_bearbeitet/js/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Samy on 17.11.2016.
+ */
+
+// Vermeidet das Javascript vorm DOM laden, ausgeführt wird
+window.addEventListener("load", function(event) {
+
+    // var map;
+    // function initMap() {
+    //     alert("drin");
+    //     map = new google.maps.Map(document.getElementById('googleMap'), {
+    //         center: {lat: -34.397, lng: 150.644},
+    //         zoom: 8
+    //     });
+    // }
+
+});
+
+
